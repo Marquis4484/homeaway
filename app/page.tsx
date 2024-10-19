49a92e2d0aaa8751dcab2b625d659ec0f9be7a5b
Marquis Sampson <marquis4484@gmail.com>
2024-10-19 14:43:47 -0400

    update to middleware

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function HomePage() {
-  return (
-    <h1 className='text 3xl'>HomePage</h1>
-  )
+  return <h1 className="text 3xl">HomeAway</h1>;
 }
 
-export default HomePage
+export default HomePage;
