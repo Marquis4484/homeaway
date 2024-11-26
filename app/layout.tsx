f389c6463ede147974284c3bb7f5429eeae99d5c
Marquis Sampson <marquis4484@gmail.com>
2024-11-26 14:05:16 -0500

    files added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-// import Navbar from '@/components/navbar/Navbar';
-// import Providers from './providers';
+import Navbar from "@/components/navbar/Navbar";
+import Providers from "./providers";
 import { ClerkProvider } from "@clerk/nextjs";
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,10 +20,10 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
         <body className={inter.className}>
-          {/* <Providers>
-            <Navbar /> */}
-          <main className="container py-10">{children}</main>
-          {/* </Providers> */}
+          <Providers>
+            <Navbar />
+            <main className="container py-10">{children}</main>
+          </Providers>
         </body>
       </html>
     </ClerkProvider>
