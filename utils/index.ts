b735909dfd2cd446c67d5b179b42fb03cef2c01d
Marquis Sampson <marquis4484@gmail.com>
2025-02-03 17:54:45 -0500

    prisma changes

--- utils/index.ts
+++ utils/index.ts
@@ -4,7 +4,7 @@ const prisma = new PrismaClient();
 
 async function main() {
    
-   const allUsers = await prisma.user.findMany()
+   const allUsers = await prisma.user:.findMany()
     console.log(allUsers)
   }
 
